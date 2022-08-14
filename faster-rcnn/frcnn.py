b4b3448ef436d7f88d5cd90217f5f25b48a69bc1
Xsmile-love <78161848+Xsmile-love@users.noreply.github.com>
2022-08-14 22:38:25 +0800

    Update frcnn.py

--- faster-rcnn/frcnn.py
+++ faster-rcnn/frcnn.py
@@ -14,43 +14,40 @@ from utils.utils_bbox import DecodeBox
 
 
 #--------------------------------------------#
-#   使用自己训练好的模型预测需要修改2个参数
-#   model_path和classes_path都需要修改！
-#   如果出现shape不匹配
-#   一定要注意训练时的NUM_CLASSES、
-#   model_path和classes_path参数的修改
+#   Prediction using your own trained model requires modification of 2 parameters
+#   Both model_path and classes_path need to be modified!
+#   If there is a shape mismatch, make sure to pay attention to the NUM_CLASSES, model_path and classes_path parameters during training
 #--------------------------------------------#
 class FRCNN(object):
     _defaults = {
         #--------------------------------------------------------------------------#
-        #   使用自己训练好的模型进行预测一定要修改model_path和classes_path！
-        #   model_path指向logs文件夹下的权值文件，classes_path指向model_data下的txt
+        #   Use your own trained model for prediction make sure to modify model_path and classes_path!
+        #   model_path points to the weights file under logs folder, classes_path points to the txt under model_data
         #
-        #   训练好后logs文件夹下存在多个权值文件，选择验证集损失较低的即可。
-        #   验证集损失较低不代表mAP较高，仅代表该权值在验证集上泛化性能较好。
-        #   如果出现shape不匹配，同时要注意训练时的model_path和classes_path参数的修改
+        #   After training there are multiple weights files in the logs folder, just choose the one with lower loss in the validation set.
+        #   A lower loss in the validation set does not mean a higher mAP, it only means that the weight has better generalization performance on the validation set.
+        #   If there is a shape mismatch, also pay attention to the modification of the model_path and classes_path parameters during training
         #--------------------------------------------------------------------------#
         "model_path"    : 'model_data/voc_weights_resnet.pth',
         "classes_path"  : 'model_data/voc_classes.txt',
         #---------------------------------------------------------------------#
-        #   网络的主干特征提取网络，resnet50或者vgg
+        #   The network's backbone feature extraction network, resnet50 or vgg
         #---------------------------------------------------------------------#
         "backbone"      : "resnet50",
         #---------------------------------------------------------------------#
-        #   只有得分大于置信度的预测框会被保留下来
+        #   Only prediction frames with scores greater than the confidence level will be retained
         #---------------------------------------------------------------------#
         "confidence"    : 0.5,
         #---------------------------------------------------------------------#
-        #   非极大抑制所用到的nms_iou大小
+        #   The size of nms_iou used for non-extreme suppression
         #---------------------------------------------------------------------#
         "nms_iou"       : 0.3,
         #---------------------------------------------------------------------#
-        #   用于指定先验框的大小
+        #   Used to specify the size of the a priori box
         #---------------------------------------------------------------------#
         'anchors_size'  : [8, 16, 32],
         #-------------------------------#
-        #   是否使用Cuda
-        #   没有GPU可以设置成False
+        #   Whether to use Cuda, no GPU can be set to False
         #-------------------------------#
         "cuda"          : True,
     }
@@ -63,7 +60,7 @@ class FRCNN(object):
             return "Unrecognized attribute name '" + n + "'"
 
     #---------------------------------------------------#
-    #   初始化faster RCNN
+    #   Initialize faster RCNN
     #---------------------------------------------------#
     def __init__(self, **kwargs):
         self.__dict__.update(self._defaults)
@@ -71,7 +68,7 @@ class FRCNN(object):
             setattr(self, name, value)
             self._defaults[name] = value 
         #---------------------------------------------------#
-        #   获得种类和先验框的数量
+        #   Get the number of types and a priori boxes
         #---------------------------------------------------#
         self.class_names, self.num_classes  = get_classes(self.classes_path)
 
@@ -81,7 +78,7 @@ class FRCNN(object):
         self.bbox_util  = DecodeBox(self.std, self.num_classes)
 
         #---------------------------------------------------#
-        #   画框设置不同的颜色
+        #   Set different colors for picture frames
         #---------------------------------------------------#
         hsv_tuples = [(x / self.num_classes, 1., 1.) for x in range(self.num_classes)]
         self.colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
@@ -91,11 +88,11 @@ class FRCNN(object):
         show_config(**self._defaults)
 
     #---------------------------------------------------#
-    #   载入模型
+    #   Load Model
     #---------------------------------------------------#
     def generate(self):
         #-------------------------------#
-        #   载入模型与权值
+        #   Loading models and weights
         #-------------------------------#
         self.net    = FasterRCNN(self.num_classes, "predict", anchor_scales = self.anchors_size, backbone = self.backbone)
         device      = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -108,28 +105,27 @@ class FRCNN(object):
             self.net = self.net.cuda()
     
     #---------------------------------------------------#
-    #   检测图片
+    #   Test pictures
     #---------------------------------------------------#
     def detect_image(self, image, crop = False, count = False):
         #---------------------------------------------------#
-        #   计算输入图片的高和宽
+        #   Calculate the height and width of the input image
         #---------------------------------------------------#
         image_shape = np.array(np.shape(image)[0:2])
         #---------------------------------------------------#
-        #   计算resize后的图片的大小，resize后的图片短边为600
+        #   Calculate the size of the image after resize, the short side of the image after resize is 600
         #---------------------------------------------------#
         input_shape = get_new_img_size(image_shape[0], image_shape[1])
         #---------------------------------------------------------#
-        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
-        #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
+        #   Converting images to RGB images here prevents grayscale maps from reporting errors when predicting. The code only supports RGB image prediction, all other types of images will be converted to RGB
         #---------------------------------------------------------#
         image       = cvtColor(image)
         #---------------------------------------------------------#
-        #   给原图像进行resize，resize到短边为600的大小上
+        #   Resize the original image, resize to a size of 600 on the short side
         #---------------------------------------------------------#
         image_data  = resize_image(image, [input_shape[1], input_shape[0]])
         #---------------------------------------------------------#
-        #   添加上batch_size维度
+        #   Add on the batch_size dimension
         #---------------------------------------------------------#
         image_data  = np.expand_dims(np.transpose(preprocess_input(np.array(image_data, dtype='float32')), (2, 0, 1)), 0)
 
@@ -139,18 +135,18 @@ class FRCNN(object):
                 images = images.cuda()
             
             #-------------------------------------------------------------#
-            #   roi_cls_locs  建议框的调整参数
-            #   roi_scores    建议框的种类得分
-            #   rois          建议框的坐标
+            #   roi_cls_locs  Suggested adjustment parameters for the box
+            #   roi_scores    Suggestion box type score
+            #   rois          Coordinates of the suggestion box
             #-------------------------------------------------------------#
             roi_cls_locs, roi_scores, rois, _ = self.net(images)
             #-------------------------------------------------------------#
-            #   利用classifier的预测结果对建议框进行解码，获得预测框
+            #   Decode the suggestion frame using the prediction result of the classifier to obtain the prediction frame
             #-------------------------------------------------------------#
             results = self.bbox_util.forward(roi_cls_locs, roi_scores, rois, image_shape, input_shape, 
                                                     nms_iou = self.nms_iou, confidence = self.confidence)
             #---------------------------------------------------------#
-            #   如果没有检测出物体，返回原图
+            #   If no object is detected, return to the original image
             #---------------------------------------------------------#           
             if len(results[0]) <= 0:
                 return image
@@ -160,12 +156,12 @@ class FRCNN(object):
             top_boxes   = results[0][:, :4]
         
         #---------------------------------------------------------#
-        #   设置字体与边框厚度
+        #   Set font and border thickness
         #---------------------------------------------------------#
         font        = ImageFont.truetype(font='model_data/simhei.ttf', size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
         thickness   = int(max((image.size[0] + image.size[1]) // np.mean(input_shape), 1))
         #---------------------------------------------------------#
-        #   计数
+        #   Counting
         #---------------------------------------------------------#
         if count:
             print("top_label:", top_label)
@@ -177,7 +173,7 @@ class FRCNN(object):
                 classes_nums[i] = num
             print("classes_nums:", classes_nums)
         #---------------------------------------------------------#
-        #   是否进行目标的裁剪
+        #   Whether to perform target tailoring
         #---------------------------------------------------------#
         if crop:
             for i, c in list(enumerate(top_label)):
@@ -194,7 +190,7 @@ class FRCNN(object):
                 crop_image.save(os.path.join(dir_save_path, "crop_" + str(i) + ".png"), quality=95, subsampling=0)
                 print("save crop_" + str(i) + ".png to " + dir_save_path)
         #---------------------------------------------------------#
-        #   图像绘制
+        #   Image plotting
         #---------------------------------------------------------#
         for i, c in list(enumerate(top_label)):
             predicted_class = self.class_names[int(c)]
@@ -229,22 +225,22 @@ class FRCNN(object):
 
     def get_FPS(self, image, test_interval):
         #---------------------------------------------------#
-        #   计算输入图片的高和宽
+        #   Calculate the height and width of the input image
         #---------------------------------------------------#
         image_shape = np.array(np.shape(image)[0:2])
         input_shape = get_new_img_size(image_shape[0], image_shape[1])
         #---------------------------------------------------------#
-        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
-        #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
+        #   Here the image is converted to RGB image to prevent the grayscale map from reporting errors in prediction.
+        #   The code only supports RGB image prediction, all other types of images will be converted to RGB
         #---------------------------------------------------------#
         image       = cvtColor(image)
         
         #---------------------------------------------------------#
-        #   给原图像进行resize，resize到短边为600的大小上
+        #   Resize the original image, resize to a size of 600 on the short side
         #---------------------------------------------------------#
         image_data  = resize_image(image, [input_shape[1], input_shape[0]])
         #---------------------------------------------------------#
-        #   添加上batch_size维度
+        #   Add on the batch_size dimension
         #---------------------------------------------------------#
         image_data  = np.expand_dims(np.transpose(preprocess_input(np.array(image_data, dtype='float32')), (2, 0, 1)), 0)
 
@@ -255,7 +251,7 @@ class FRCNN(object):
 
             roi_cls_locs, roi_scores, rois, _ = self.net(images)
             #-------------------------------------------------------------#
-            #   利用classifier的预测结果对建议框进行解码，获得预测框
+            #   Decode the suggestion frame using the prediction result of the classifier to obtain the prediction frame
             #-------------------------------------------------------------#
             results = self.bbox_util.forward(roi_cls_locs, roi_scores, rois, image_shape, input_shape, 
                                                     nms_iou = self.nms_iou, confidence = self.confidence)
@@ -264,7 +260,7 @@ class FRCNN(object):
             with torch.no_grad():
                 roi_cls_locs, roi_scores, rois, _ = self.net(images)
                 #-------------------------------------------------------------#
-                #   利用classifier的预测结果对建议框进行解码，获得预测框
+                #   Decode the suggestion frame using the prediction result of the classifier to obtain the prediction frame
                 #-------------------------------------------------------------#
                 results = self.bbox_util.forward(roi_cls_locs, roi_scores, rois, image_shape, input_shape, 
                                                         nms_iou = self.nms_iou, confidence = self.confidence)
@@ -274,27 +270,27 @@ class FRCNN(object):
         return tact_time
 
     #---------------------------------------------------#
-    #   检测图片
+    #  Testing pictures
     #---------------------------------------------------#
     def get_map_txt(self, image_id, image, class_names, map_out_path):
         f = open(os.path.join(map_out_path, "detection-results/"+image_id+".txt"),"w")
         #---------------------------------------------------#
-        #   计算输入图片的高和宽
+        #   Calculate the height and width of the input image
         #---------------------------------------------------#
         image_shape = np.array(np.shape(image)[0:2])
         input_shape = get_new_img_size(image_shape[0], image_shape[1])
         #---------------------------------------------------------#
-        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
-        #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
+        #   Here the image is converted to RGB image to prevent the grayscale map from reporting errors in prediction.
+        #   The code only supports RGB image prediction, all other types of images will be converted to RGB
         #---------------------------------------------------------#
         image       = cvtColor(image)
         
         #---------------------------------------------------------#
-        #   给原图像进行resize，resize到短边为600的大小上
+        #   Resize the original image, resize to a size of 600 on the short side
         #---------------------------------------------------------#
         image_data  = resize_image(image, [input_shape[1], input_shape[0]])
         #---------------------------------------------------------#
-        #   添加上batch_size维度
+        #   Add on the batch_size dimension
         #---------------------------------------------------------#
         image_data  = np.expand_dims(np.transpose(preprocess_input(np.array(image_data, dtype='float32')), (2, 0, 1)), 0)
 
@@ -305,12 +301,12 @@ class FRCNN(object):
 
             roi_cls_locs, roi_scores, rois, _ = self.net(images)
             #-------------------------------------------------------------#
-            #   利用classifier的预测结果对建议框进行解码，获得预测框
+            #   Decode the suggestion frame using the prediction result of the classifier to obtain the prediction frame
             #-------------------------------------------------------------#
             results = self.bbox_util.forward(roi_cls_locs, roi_scores, rois, image_shape, input_shape, 
                                                     nms_iou = self.nms_iou, confidence = self.confidence)
             #--------------------------------------#
-            #   如果没有检测到物体，则返回原图
+            #   If no object is detected, the original image is returned
             #--------------------------------------#
             if len(results[0]) <= 0:
                 return 
