c67a940520e2305c82150b819b0e9b2a34bf05a9
Xsmile-love <78161848+Xsmile-love@users.noreply.github.com>
2022-08-14 23:01:35 +0800

    Update utils_bbox.py

--- faster-rcnn/utils/utils_bbox.py
+++ faster-rcnn/utils/utils_bbox.py
@@ -38,7 +38,7 @@ class DecodeBox():
 
     def frcnn_correct_boxes(self, box_xy, box_wh, input_shape, image_shape):
         #-----------------------------------------------------------------#
-        #   把y轴放前面是因为方便预测框和图像的宽高进行相乘
+        #   The y-axis is put in front because it is convenient to multiply the width and height of the prediction frame and the image.
         #-----------------------------------------------------------------#
         box_yx = box_xy[..., ::-1]
         box_hw = box_wh[..., ::-1]
@@ -59,28 +59,28 @@ class DecodeBox():
         #--------------------------------#
         rois    = rois.view((bs, -1, 4))
         #----------------------------------------------------------------------------------------------------------------#
-        #   对每一张图片进行处理，由于在predict.py的时候，我们只输入一张图片，所以for i in range(len(mbox_loc))只进行一次
+        #   For each image, since in predict.py we only input one image, so for i in range(len(mbox_loc)) is done only once
         #----------------------------------------------------------------------------------------------------------------#
         for i in range(bs):
             #----------------------------------------------------------#
-            #   对回归参数进行reshape
+            #   Perform reshape on regression parameters
             #----------------------------------------------------------#
             roi_cls_loc = roi_cls_locs[i] * self.std
             #----------------------------------------------------------#
-            #   第一维度是建议框的数量，第二维度是每个种类
-            #   第三维度是对应种类的调整参数
+            #   The first dimension is the number of suggestion boxes, and the second dimension is the number of each category
+            #   The third dimension is the adjustment parameter for the corresponding category
             #----------------------------------------------------------#
             roi_cls_loc = roi_cls_loc.view([-1, self.num_classes, 4])
 
             #-------------------------------------------------------------#
-            #   利用classifier网络的预测结果对建议框进行调整获得预测框
+            #   Using the prediction results of the classifier network to adjust the suggestion frame to obtain the prediction frame
             #   num_rois, 4 -> num_rois, 1, 4 -> num_rois, num_classes, 4
             #-------------------------------------------------------------#
             roi         = rois[i].view((-1, 1, 4)).expand_as(roi_cls_loc)
             cls_bbox    = loc2bbox(roi.contiguous().view((-1, 4)), roi_cls_loc.contiguous().view((-1, 4)))
             cls_bbox    = cls_bbox.view([-1, (self.num_classes), 4])
             #-------------------------------------------------------------#
-            #   对预测框进行归一化，调整到0-1之间
+            #   Normalize the prediction frame and adjust to between 0 and 1
             #-------------------------------------------------------------#
             cls_bbox[..., [0, 2]] = (cls_bbox[..., [0, 2]]) / input_shape[1]
             cls_bbox[..., [1, 3]] = (cls_bbox[..., [1, 3]]) / input_shape[0]
@@ -91,15 +91,15 @@ class DecodeBox():
             results.append([])
             for c in range(1, self.num_classes):
                 #--------------------------------#
-                #   取出属于该类的所有框的置信度
-                #   判断是否大于门限
+                #   Take the confidence of all the boxes belonging to the class
+                #   Determine if it is greater than the threshold
                 #--------------------------------#
                 c_confs     = prob[:, c]
                 c_confs_m   = c_confs > confidence
 
                 if len(c_confs[c_confs_m]) > 0:
                     #-----------------------------------------#
-                    #   取出得分高于confidence的框
+                    #   Take out the boxes with scores higher than confidence
                     #-----------------------------------------#
                     boxes_to_process = cls_bbox[c_confs_m, c]
                     confs_to_process = c_confs[c_confs_m]
@@ -110,16 +110,16 @@ class DecodeBox():
                         nms_iou
                     )
                     #-----------------------------------------#
-                    #   取出在非极大抑制中效果较好的内容
+                    #   Take out the content that is more effective in non-extreme suppression
                     #-----------------------------------------#
                     good_boxes  = boxes_to_process[keep]
                     confs       = confs_to_process[keep][:, None]
                     labels      = (c - 1) * torch.ones((len(keep), 1)).cuda() if confs.is_cuda else (c - 1) * torch.ones((len(keep), 1))
                     #-----------------------------------------#
-                    #   将label、置信度、框的位置进行堆叠。
+                    #   Stack the positions of the labels, confidence levels, and boxes.
                     #-----------------------------------------#
                     c_pred      = torch.cat((good_boxes, confs, labels), dim=1).cpu().numpy()
-                    # 添加进result里
+                    # Add to the result
                     results[-1].extend(c_pred)
 
             if len(results[-1]) > 0:
