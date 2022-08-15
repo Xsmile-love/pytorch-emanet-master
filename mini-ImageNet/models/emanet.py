02c10d1b303525239af52a11c25ccea6d925c8e1
Xsmile-love <78161848+Xsmile-love@users.noreply.github.com>
2022-08-15 09:00:53 +0800

    Update emanet.py

--- mini-ImageNet/models/emanet.py
+++ mini-ImageNet/models/emanet.py
@@ -22,6 +22,9 @@ def conv1x1(in_planes, out_planes, stride=1):
 
 
 class Residual(nn.Module):
+    '''
+    Residual moduale
+    '''
     def __init__(self, fn):
         super().__init__()
         self.fn = fn
@@ -31,6 +34,12 @@ class Residual(nn.Module):
 
 
 class _BatchAttNorm(_BatchNorm):
+    """
+    The softmax function is used to recalibrate the channel weight vector to
+    enhance the channel modeling ability of SoftBAN and improve the network performance.
+    Args:
+        num_features: Number of input feature map channels
+    """
     def __init__(self, num_features, eps=1e-5, momentum=0.1, affine=False):
         super(_BatchAttNorm, self).__init__(num_features, eps, momentum, affine)
         self.avg = nn.AdaptiveAvgPool2d((1, 1))
@@ -43,12 +52,13 @@ class _BatchAttNorm(_BatchNorm):
         self.bias_readjust.data.fill_(-1)
         self.weight.data.fill_(1)
         self.bias.data.fill_(0)
+        # parameter recalibration
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, input):
         self._check_input_dim(input)
 
-        # Batch norm
+        # Soft Batch norm
         attention = self.sigmoid(self.avg(input) * self.weight_readjust + self.bias_readjust)
         bn_w = self.weight * self.softmax(attention)
 
@@ -67,18 +77,26 @@ class BAN2d(_BatchAttNorm):
 
 
 def ConvMixer1(in_planes, out_planes, kernel_size=3, mixer_size=9, stride=1, padding=1, dilation=1, groups=1):
+     """
+    CNN-style transformer architecture
+    in_planes: input channel
+    out_planes: out channel
+    mixer_size: spatial mixing kernel size
+    """
     return nn.Sequential(
         nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding,
                   dilation=dilation, groups=groups, bias=False),
         nn.GELU(),
         nn.BatchNorm2d(out_planes),
         *[nn.Sequential(
+                # spatial mixture
                 Residual(nn.Sequential(
                     nn.GroupNorm(out_planes, out_planes),
                     nn.Conv2d(out_planes, out_planes, mixer_size, groups=out_planes, padding="same"),
                     nn.GELU(),
                     nn.BatchNorm2d(out_planes)
                 )),
+                # channel mixture
                 Residual(nn.Sequential(
                     BAN2d(out_planes),
                     nn.Conv2d(out_planes, out_planes, kernel_size=1),
@@ -90,7 +108,13 @@ def ConvMixer1(in_planes, out_planes, kernel_size=3, mixer_size=9, stride=1, pad
 
 
 class MAModule(nn.Module):
-
+    """
+    MA module
+    inplans: input channel
+    outplans: output channel
+    conv_kernals: different branches convolutional kernel size
+    conv_groups: different branches convolutional groups size
+    """
     def __init__(self, inplans, planes, conv_kernels=[3, 5, 7, 9], stride=1, conv_groups=[1, 4, 8, 16]):
         super(MAModule, self).__init__()
         self.conv_1 = ConvMixer1(inplans, planes//4, kernel_size=conv_kernels[0], padding=conv_kernels[0]//2,
@@ -111,18 +135,24 @@ class MAModule(nn.Module):
         x2 = self.conv_2(x)
         x3 = self.conv_3(x)
         x4 = self.conv_4(x)
+        
+        # splicing of different branches
         feats = torch.cat((x1, x2, x3, x4), dim=1)     # （b,c,h,w）
         feats = feats.view(batch_size, 4, self.split_channel, feats.shape[2], feats.shape[3])
-
+        
+        # calculate different branch weights
         x1_se = self.se(x1)
         x2_se = self.se(x2)
         x3_se = self.se(x3)
         x4_se = self.se(x4)
-
+        
+        # re-correct different branch weights and multiply them with the corresponding channel feature maps
         x_se = torch.cat((x1_se, x2_se, x3_se, x4_se), dim=1)      
         attention_vectors = x_se.view(batch_size, 4, self.split_channel, 1, 1)
         attention_vectors = self.softmax(attention_vectors)
         feats_weight = feats * attention_vectors
+        
+        # (B,4,C/4,H,W)-->(B,C,H,W)
         for i in range(4):
             x_se_weight_fp = feats_weight[:, i, :, :]
             if i == 0:
@@ -134,6 +164,7 @@ class MAModule(nn.Module):
 
 
 class EMABlock(nn.Module):
+    """EMA Bottleneck bloch,the MA module is used to replace the 3x3 convolution in the ResNet residual block"""
     expansion = 4
 
     def __init__(self, inplanes, planes, stride=1, downsample=None, norm_layer=None, conv_kernels=[3, 5, 7, 9],
@@ -188,7 +219,8 @@ class EMANet(nn.Module):
         self.layer4 = self._make_layers(block, 512, layers[3], stride=2, groups=[16, 16, 16, 16])
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512 * block.expansion, num_classes)
-
+        
+        # Initialization layers
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
@@ -233,11 +265,13 @@ class EMANet(nn.Module):
 
 
 def emanet50():
+    '''Construction of emanet50 network'''
     model = EMANet(EMABlock, [3, 4, 6, 3], num_classes=100)
     return model
 
 
 def emanet101():
+    '''Construction of emanet101 network'''
     model = EMANet(EMABlock, [3, 4, 23, 3], num_classes=100)
     return model
 
